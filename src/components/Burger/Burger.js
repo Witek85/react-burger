0d3ca99aba4b471fd9b8e24fd6c3d649018a19e7
Witek85 <witold.szklarski@gmail.com>
2019-01-05 00:10:00 +0100

    reduce from empty arrays

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,7 +8,15 @@ const Burger = ( props ) => {
         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
             return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
         });
-    });
+    }).reduce((arr, el) => {
+        console.log(arr);
+        console.log(el);
+        return arr.concat(el);
+    }, []);
+    console.log(transformedIngredients);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
