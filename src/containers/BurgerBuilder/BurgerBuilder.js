8585eb95c30490a48979cc0db293249e33e0ada0
Witek85 <witold.szklarski@gmail.com>
2019-01-05 14:00:52 +0100

    build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   constructor(props) {
@@ -18,7 +19,7 @@ class BurgerBuilder extends Component {
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </React.Fragment>
     );
   }
