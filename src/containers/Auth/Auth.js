2ae45d2c455b9e1dc6df029221a583553582ee0d
Witek85 <witold.szklarski@gmail.com>
2019-01-20 00:15:37 +0100

    auth actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import RegisterForm from './RegisterForm/RegisterForm'
+import RegisterForm from './RegisterForm/RegisterForm';
+import * as actions from '../../store/actions';
 
 class Auth extends Component {
 
   registerHandler = values => {
-    console.log(values);
+    console.log(values.username, values.password);
+    this.props.onAuth(values.username, values.password);
   }
 
   render () {
@@ -17,4 +20,20 @@ class Auth extends Component {
   }
 };
 
-export default Auth;
+
+// const mapStateToProps = state => {
+//   return {
+//     ings: state.burgerBuilder.ingredients,
+//     price: state.burgerBuilder.totalPrice,
+//     loading: state.order.loading
+//   }
+// }
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+      onAuth: (email, password) => dispatch(actions.auth(email, password))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
